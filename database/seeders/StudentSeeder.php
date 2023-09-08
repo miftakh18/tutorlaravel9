a6f98466906678ca7399796edf364e69e37cd3cd
project1 <mmiftakh18@gmail.com>
2023-09-09 06:32:36 +0700

    tutorial seeder

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            ['id' => 1, 'name' => 'Miftah', 'score' => 80],
+            ['id' => 2, 'name' => 'Aldan', 'score' => 85],
+            ['id' => 3, 'name' => 'Lisan', 'score' => 70],
+            ['id' => 4, 'name' => 'ojan', 'score' => 90]
+        ];
+        DB::table('students')->insert($data);
+    }
+}
